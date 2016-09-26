fb74f59b2b34a8f6b9528c643b0e738e1f30b9ab
viparthasarathy <viparthasarathy@gmail.com>
2016-09-26 13:33:56 -0700

    Add Question and Choice models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(model.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(model.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -6,5 +6,3 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-
-    
